c2c3730f595aecf39faaf2804f31b25b5bf5cdf4
Safonik <valera.safonik@gmail.com>
2017-12-25 19:46:45 +0200

    Initial commit

---
+++ academy/src/academy_admin/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class AcademyAdminConfig(AppConfig):
+    name = 'academy_admin'
---
+++ academy/src/academy_site/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class AcademySiteConfig(AppConfig):
+    name = 'academy_site'
